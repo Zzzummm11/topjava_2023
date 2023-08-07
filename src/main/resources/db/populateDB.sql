ba3662e763ce3030030ff6638252cac856b6d031
Konstantin Mogilev <zzzummm272@gmail.com>
2023-08-08 00:22:26 +0400

    4_2_HW3_optional

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -12,3 +12,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meal (date_time, description, calories, user_id)
+VALUES ('2023-01-29 10:00:00', 'Admin breakfast', 500, 100001),
+       ('2023-01-30 13:00:00', 'Admin lunch', 1000, 100001),
+       ('2023-01-30 20:00:00', 'Admin dinner', 500, 100001),
+       ('2023-01-31 00:00:00', 'Border meals', 100, 100001),
+       ('2023-01-31 10:00:00', 'Admin breakfast', 1000, 100001),
+       ('2023-01-31 13:00:00', 'Admin lunch', 500, 100001),
+       ('2023-02-01 10:00:00', 'Admin breakfast', 410, 100001),
+       ('2023-01-30 10:00:00', 'User breakfast', 120, 100000),
+       ('2023-01-30 13:00:00', 'User lunch', 300, 100000),
+       ('2023-01-30 20:00:00', 'User dinner', 200, 100000);
