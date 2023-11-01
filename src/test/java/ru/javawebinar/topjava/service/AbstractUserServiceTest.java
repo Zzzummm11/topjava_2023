a3bda4fc9f43a42f3f123277c439520b8bd93a96
Konstantin Mogilev <zzzummm272@gmail.com>
2023-11-01 23:27:46 +0400

    HW06: opt2 - fix after second code review

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
@@ -20,20 +19,6 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Autowired
     protected UserService service;
-
-    @Autowired
-    private CacheManager cacheManager;
-
-//    @Autowired(required = false)
-//    protected JpaUtil jpaUtil;
-
-//    @Before
-//    public void setup() {
-//        cacheManager.getCache("users").clear();
-//        if (env.acceptsProfiles(Profiles.of(JPA, DATAJPA))) {
-//            jpaUtil.clear2ndLevelHibernateCache();
-//        }
-//    }
 
     @Test
     public void create() {
